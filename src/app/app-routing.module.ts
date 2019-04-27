006f4c9c335243c722bc7571f34f8b7c492e2e3d
wellwind <wellwind@mail2000.com.tw>
2019-04-27 09:59:29 +0800

    完成任務 02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { MainComponent } from './layout/main/main.component';
+import { EditorComponent } from './posts/editor/editor.component';
+import { PostComponent } from './posts/post/post.component';
+import { PostsComponent } from './posts/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'posts',
+        pathMatch: 'full'
+      },
+      {
+        path: 'posts',
+        component: PostsComponent
+      },
+      {
+        path: 'post/:id',
+        component: PostComponent
+      },
+      {
+        path: 'create',
+        component: EditorComponent
+      }
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { LayoutModule } from './layout/layout.module';
+import { PostsModule } from './posts/posts.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LayoutModule, AppRoutingModule],
+  imports: [BrowserModule, LayoutModule, AuthModule, PostsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
