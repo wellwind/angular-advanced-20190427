006789896eec8f0ae5542ff0c6e888c5c49a7062
wellwind <wellwind@mail2000.com.tw>
2019-04-27 16:02:35 +0800

    完成任務09

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
+  form = new FormGroup({
+    title: new FormControl('hello'),
+    body: new FormControl('test'),
+    tags: new FormArray([
+      new FormControl('Angular'),
+      new FormControl('React'),
+      new FormControl('Vue')
+    ])
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  get tags(): FormArray {
+    return this.form.get('tags') as FormArray;
   }
 
+  constructor() {}
+
+  ngOnInit() {}
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+  addTags(tagInput: HTMLInputElement) {
+    tagInput.value.split(',')
+    .map(tagName => tagName.trim())
+    .filter(tagName => !!tagName)
+    .filter(tagName => (this.tags.value as string[]).indexOf(tagName) === -1)
+    .forEach(tagName => {
+      this.tags.push(new FormControl(tagName));
+    });
+    tagInput.value = '';
+    tagInput.focus();
+  }
 }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { PostsRoutingModule } from './posts-routing.module';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { EditorComponent } from './editor/editor.component';
 import { PostComponent } from './post/post.component';
+import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts/posts.component';
+
 
 @NgModule({
   declarations: [EditorComponent, PostComponent, PostsComponent],
   imports: [
     CommonModule,
-    PostsRoutingModule
+    PostsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class PostsModule { }
